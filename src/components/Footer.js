bbb4114ff3b4a2943ba8e47438d5bb157791e833
Andrew Ryden <andrew.ryden@acadaca.com>
2020-03-07 00:52:06 -0500

    Strip Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,33 +20,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/locations">
-                        Order Online
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
+              <div className="column is-10 social">
                 <a title="facebook" href="https://facebook.com">
                   <img
                     src={facebook}
