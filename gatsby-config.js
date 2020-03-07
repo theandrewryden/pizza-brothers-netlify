34c6fbb27ce3d0faa0c7f1a5f807199fa921ed36
Andrew Ryden <andrew.ryden@acadaca.com>
2020-03-06 23:43:56 -0500

    Update config metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Pizza Brothers of New Jersey',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Pizza Brothers of New Jersey',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
