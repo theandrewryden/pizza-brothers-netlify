0ff24d017bfa189eadc8529e13f752240b481d64
Andrew Ryden <andrew.ryden@acadaca.com>
2020-03-14 23:25:11 -0400

    Order Online button

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -12,25 +12,46 @@ class LocationGrid extends React.Component {
             <div className="columns is-multiline">
                 {locations &&
                   locations.map(({ node: location }) => (
-                    <div key={location.id} className="column is-6">
-                        <section className="section">
-                            <div className="has-text-centered location">
-                                <div
-                                    style={{
-                                        width: '240px',
-                                        display: 'inline-block',
-                                    }}
-                                >
-                                    <div className="location-title">
-                                        <h2 className="has-text-light">{location.frontmatter.title}</h2>
-                                    </div>
-                                    <div className="details">
-                                        <p className="has-text-light location-address">{location.frontmatter.address}</p>
-                                        <p className="has-text-light location-phone">{location.frontmatter.phone}</p>
+                    <div key={location.id} className="column is-12">
+                        <div className="column is-6" style={{display: 'inline-block', textAlign: 'left'}}>
+                            <iframe
+                                src={location.frontmatter.mapEmbedUrl}
+                                width={400}
+                                height={300}
+                                marginWidth={0}
+                                marginHeight={0}
+                                frameBorder={0}
+                                style={{border: 50}}
+                                allowFullScreen={false}
+                                aria-hidden={false}
+                                tabIndex={0}/>
+                        </div>
+                        <div className="column is-6" style={{
+                            textAlign: 'right',
+                            verticalAlign: 'top',
+                            display: 'inline-block'
+                        }}>
+                            <section className="section">
+                                <div className="location">
+                                    <div>
+                                        <div className="location-title">
+                                            <h2 className="has-text-light">{location.frontmatter.title}</h2>
+                                        </div>
+                                        <div className="details">
+                                            <p className="has-text-light location-address">{location.frontmatter.address}</p>
+                                            <p className="has-text-light location-phone">{location.frontmatter.phone}</p>
+                                        </div>
+                                        <div className="actions">
+                                            <button
+                                                title="Order Online"
+                                                onClick={() => { window.location.href = location.frontmatter.onlineOrderingUrl }}>
+                                                Order Online
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </section>
+                            </section>
+                        </div>
                     </div>
                 ))}
             </div>
